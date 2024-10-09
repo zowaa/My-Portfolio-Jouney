36c22bc5db016670cd6575986ff635d99bfe26df
zowa <elfilfatimaezzahra0@gmail.com>
2024-10-09 12:31:47 +0100

    fetch global function used , for better readability

--- src/Components/Weather.tsx
+++ src/Components/Weather.tsx
@@ -2,8 +2,13 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
+//keys to remove after fetching in the backend
+const ipdataAPI_key = import.meta.env.VITE_IPDATA_API;
+const weatherAPI_key = import.meta.env.VITE_WEATHERAPI_API;
+const opencageAPI_key = import.meta.env.VITE_OPENCAGE_API;
+
 interface WeatherInfo {
-  location: { name: string };
+  location: { name: string }; // to delete and use city instead
   current: { temp_c: number; condition: { text: string; icon: string } };
 }
 
@@ -14,124 +19,86 @@ const Weather = () => {
   const [isError, setIsError] = useState(false);
   const [city, setCity] = useState<string>("");
 
-  const ipdataAPI_key = import.meta.env.VITE_IPDATA_API;
-  const weatherAPI_key = import.meta.env.VITE_WEATHERAPI_API;
-  const opencageAPI_key = import.meta.env.VITE_OPENCAGE_API;
+  const fetchData = async ({
+    url,
+    code,
+    name,
+  }: {
+    url: string;
+    code: number;
+    name: string;
+  }) => {
+    try {
+      const response = await axios.get(url);
+      if (response.status === 200) return response.data;
+      else if (response.status === code) {
+        setIsError(true);
+        console.log(`Error in ${name} fetch: Quota excessed`);
+      } else {
+        setIsError(true);
+        console.log(
+          `Error in ${name} fetch: Received status code ${response.status}`,
+        );
+      }
+    } catch (err) {
+      setIsError(true);
+      console.log(`Error in ${name} fetch: ${err}`);
+    }
+  };
 
   useEffect(() => {
     const fetchWeather = async () => {
-      let locationData: { latitude?: number; longitude?: number } = {};
-
       setIsError(false);
       setIsLoading(true);
 
-      //fetch location with ipdata API
       try {
-        const url = `https://api.ipdata.co?api-key=${ipdataAPI_key}`;
-        const response = await axios.get(url);
-        if (response.status === 200) {
-          const data = response.data;
-          if (!data || !data.latitude || !data.longitude) {
-            setIsError(true);
-            console.log(
-              `Error in location fetch: Data object Empty, no data returned`,
-            );
-          } else {
-            locationData = data;
-          }
-        } else if (response.status === 403) {
+        //fetch location with ipdata API
+        const locationData = await fetchData({
+          url: `https://api.ipdata.co?api-key=${ipdataAPI_key}`,
+          code: 403,
+          name: "location",
+        });
+
+        if (!locationData?.latitude || !locationData?.longitude) {
           setIsError(true);
-          console.log(`Error in location fetch: Quota excessed`);
-        } else {
-          setIsError(true);
-          console.log(
-            `Error in location fetch: Received status code ${response.status}`,
-          );
+          return;
         }
+
+        const { latitude, longitude } = locationData;
+
+        //fetch city name with opencage API
+        const cityData = await fetchData({
+          url: `https://api.opencagedata.com/geocode/v1/json?q=${latitude},${longitude}&key=${opencageAPI_key}&language=${i18n.language}`,
+          code: 429,
+          name: "city",
+        });
+
+        if (!cityData?.results[0]?.components?.city) {
+          setIsError(true);
+          return;
+        }
+
+        setCity(cityData.results[0].components.city);
+
+        //fetch weather with weatherapi API
+        const weatherData = await fetchData({
+          url: `https://api.weatherapi.com/v1/current.json?key=${weatherAPI_key}&q=${latitude},${longitude}&lang=${i18n.language}`,
+          code: 403,
+          name: "weather",
+        });
+
+        if (
+          !weatherData?.current?.condition?.text ||
+          !weatherData?.current?.condition?.icon
+        ) {
+          setIsError(true);
+          return;
+        }
+
+        setWeatherInfo(weatherData);
       } catch (err) {
         setIsError(true);
-        console.log(`Error in location fetch: ${err}`);
-      }
-
-      if (isError) {
-        setIsLoading(false);
-        return;
-      }
-
-      //fetch city name with opencage API
-      try {
-        const url = `https://api.opencagedata.com/geocode/v1/json?q=${locationData.latitude},${locationData.longitude}&key=${opencageAPI_key}&language=${i18n.language}`;
-        const response = await axios.get(url);
-        if (response.status === 200) {
-          const data = response.data;
-          if (
-            !data ||
-            !data.results ||
-            !data.results[0] ||
-            !data.results[0].components ||
-            !data.results[0].components.city
-          ) {
-            setIsError(true);
-            console.log(
-              `Error in city fetch: Data object Empty, no data returned`,
-            );
-          } else {
-            setCity(data.results[0].components.city);
-          }
-        } else if (response.status === 429) {
-          setIsError(true);
-          console.log(`Error in city fetch: Quota excessed`);
-        } else {
-          setIsError(true);
-          console.log(
-            `Error in city fetch: Received status code ${response.status}`,
-          );
-        }
-      } catch (err) {
-        setIsError(true);
-        console.log(`Error in city fetch: ${err}`);
-      }
-
-      if (isError) {
-        setIsLoading(false);
-        return;
-      }
-
-      //fetch weather with weatherapi API
-      try {
-        const url = `https://api.weatherapi.com/v1/current.json?key=${weatherAPI_key}&q=${locationData.latitude},${locationData.longitude}&lang=${i18n.language}`;
-        const response = await axios.get(url);
-        if (response.status === 200) {
-          const data = response.data;
-          if (
-            !data ||
-            !data.location ||
-            !data.current ||
-            !data.current.temp_c ||
-            !data.current.condition ||
-            !data.current.condition.text ||
-            !data.current.condition.icon ||
-            !data.location.name
-          ) {
-            setIsError(true);
-            console.log(
-              `Error in weather fetch: Data object Empty, no data returned`,
-            );
-          } else {
-            setWeatherInfo(data);
-          }
-        } else if (response.status === 403) {
-          setIsError(true);
-          console.log(`Error in weather fetch: Quota excessed`);
-        } else {
-          setIsError(true);
-          console.log(
-            `Error in weather fetch: Received status code ${response.status}`,
-          );
-        }
-      } catch (err) {
-        setIsError(true);
-        console.log(`Error in weather fetch: ${err}`);
+        console.log(`Error in fetchWeather: ${err}`);
       } finally {
         setIsLoading(false);
       }
